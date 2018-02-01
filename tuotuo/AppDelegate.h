2d828b94b5c857b735b18dc3b880d5a2eabbde4c
Wu <wuzs@linkstec.com>
2018-02-01 10:44:05 +0800

    Initial Commit

---
+++ tuotuo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tuotuo
+//
+//  Created by Wu on 2/1.
+//  Copyright © 2018年 Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tuotuo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tuotuo
+//
+//  Created by Wu on 2/1.
+//  Copyright © 2018年 Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
